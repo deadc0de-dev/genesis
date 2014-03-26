11e6c156d64343648fa70f9cb01262aee88d5883
labes <0x1abe5@gmail.com>
2014-03-26 22:52:56 +0100

    moved mock object classes into test class

--- src/test/java/dev/deadc0de/genesis/GenerationContextTest.java
+++ src/test/java/dev/deadc0de/genesis/GenerationContextTest.java
@@ -1,6 +1,7 @@
 package dev.deadc0de.genesis;
 
 import java.util.Collections;
+import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.Assert;
 import org.junit.Test;
@@ -23,20 +24,20 @@ public class GenerationContextTest {
 
     @Test
     public void passesItselfAsServiceGeneratorToTheFactoryMatchingTheServiceNameAndType() {
-        final CapturingServiceFactory<Object> factory = new CapturingServiceFactory<>("name", Object.class);
+        final SpyServiceFactory<Object> factory = new SpyServiceFactory<>("name", Object.class);
         final GenerationContext context = new GenerationContext(Stream.of(factory));
         final ServiceDescriptor service = new ServiceDescriptor("name", Collections.emptyMap(), Collections.emptyMap());
         context.generate(Object.class, service);
-        Assert.assertEquals(context, factory.capturedGenerator().get());
+        Assert.assertEquals(context, factory.capturedGenerator.get());
     }
 
     @Test
     public void passesTheGivenServiceDescriptorToTheFactoryMatchingTheServiceNameAndType() {
-        final CapturingServiceFactory<Object> factory = new CapturingServiceFactory<>("name", Object.class);
+        final SpyServiceFactory<Object> factory = new SpyServiceFactory<>("name", Object.class);
         final GenerationContext context = new GenerationContext(Stream.of(factory));
         final ServiceDescriptor service = new ServiceDescriptor("name", Collections.emptyMap(), Collections.emptyMap());
         context.generate(Object.class, service);
-        Assert.assertEquals(service, factory.capturedDescriptor().get());
+        Assert.assertEquals(service, factory.capturedDescriptor.get());
     }
 
     @Test
@@ -47,5 +48,51 @@ public class GenerationContextTest {
         final ServiceDescriptor service = new ServiceDescriptor("name", Collections.emptyMap(), Collections.emptyMap());
         final Object generatedService = context.generate(Object.class, service);
         Assert.assertEquals(expected, generatedService);
+    }
+
+    private static class DummyServiceFactory<S> extends AbstractServiceFactory<S> {
+
+        public DummyServiceFactory(String serviceName, Class<S> serviceType) {
+            super(serviceName, serviceType);
+        }
+
+        @Override
+        public S create(ServiceGenerator serviceGenerator, ServiceDescriptor service) {
+            throw new UnsupportedOperationException("dummy implementation");
+        }
+    }
+
+    private static class StubServiceFactory<S> extends AbstractServiceFactory<S> {
+
+        private final S service;
+
+        public StubServiceFactory(S service, String serviceName, Class<S> serviceType) {
+            super(serviceName, serviceType);
+            this.service = service;
+        }
+
+        @Override
+        public S create(ServiceGenerator serviceGenerator, ServiceDescriptor serviceDescriptor) {
+            return service;
+        }
+    }
+
+    private static class SpyServiceFactory<S> extends AbstractServiceFactory<S> {
+
+        public Optional<ServiceGenerator> capturedGenerator;
+        public Optional<ServiceDescriptor> capturedDescriptor;
+
+        public SpyServiceFactory(String serviceName, Class<S> serviceType) {
+            super(serviceName, serviceType);
+            capturedGenerator = Optional.empty();
+            capturedDescriptor = Optional.empty();
+        }
+
+        @Override
+        public S create(ServiceGenerator serviceGenerator, ServiceDescriptor serviceDescriptor) {
+            capturedGenerator = Optional.of(serviceGenerator);
+            capturedDescriptor = Optional.of(serviceDescriptor);
+            return null;
+        }
     }
 }
