69881c7652858b2ff0c0495208c872fd30f07678
labes <0x1abe5@gmail.com>
2014-06-23 01:01:22 +0200

    test refactoring

--- src/test/java/dev/deadc0de/genesis/GenerationContextTest.java
+++ src/test/java/dev/deadc0de/genesis/GenerationContextTest.java
@@ -1,6 +1,5 @@
 package dev.deadc0de.genesis;
 
-import java.util.Collections;
 import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.Assert;
@@ -8,45 +7,44 @@ import org.junit.Test;
 
 public class GenerationContextTest {
 
+    private static final String SERVICE_NAME = "service";
+    private static final ServiceDescriptor SERVICE = ServiceDescriptor.notParameterized(SERVICE_NAME);
+
     @Test(expected = IllegalArgumentException.class)
     public void providingServiceFactoriesWithSameNameAndTypeThrows() {
-        final ServiceFactory one = new DummyServiceFactory("service", Object.class);
-        final ServiceFactory other = new DummyServiceFactory("service", Object.class);
+        final ServiceFactory one = new DummyServiceFactory(SERVICE_NAME, Object.class);
+        final ServiceFactory other = new DummyServiceFactory(SERVICE_NAME, Object.class);
         final GenerationContext notCreated = new GenerationContext(Stream.of(one, other));
     }
 
     @Test(expected = IllegalStateException.class)
     public void generatingAServiceWithoutTheAssociatedFactoryThrows() {
         final GenerationContext context = new GenerationContext(Stream.empty());
-        final ServiceDescriptor service = new ServiceDescriptor("service", Collections.emptyMap(), Collections.emptyMap());
-        context.generate(Object.class, service);
+        context.generate(Object.class, SERVICE);
     }
 
     @Test
     public void passesItselfAsServiceGeneratorToTheFactoryMatchingTheServiceNameAndType() {
-        final SpyServiceFactory factory = new SpyServiceFactory("service", Object.class);
+        final SpyServiceFactory factory = new SpyServiceFactory(SERVICE_NAME, Object.class);
         final GenerationContext context = new GenerationContext(Stream.of(factory));
-        final ServiceDescriptor service = new ServiceDescriptor("service", Collections.emptyMap(), Collections.emptyMap());
-        context.generate(Object.class, service);
+        context.generate(Object.class, SERVICE);
         Assert.assertEquals(context, factory.capturedGenerator.get());
     }
 
     @Test
     public void passesTheGivenServiceDescriptorToTheFactoryMatchingTheServiceNameAndType() {
-        final SpyServiceFactory factory = new SpyServiceFactory("service", Object.class);
+        final SpyServiceFactory factory = new SpyServiceFactory(SERVICE_NAME, Object.class);
         final GenerationContext context = new GenerationContext(Stream.of(factory));
-        final ServiceDescriptor service = new ServiceDescriptor("service", Collections.emptyMap(), Collections.emptyMap());
-        context.generate(Object.class, service);
-        Assert.assertEquals(service, factory.capturedDescriptor.get());
+        context.generate(Object.class, SERVICE);
+        Assert.assertEquals(SERVICE, factory.capturedDescriptor.get());
     }
 
     @Test
     public void returnsTheServiceGeneratedByTheFactoryMatchingTheServiceNameAndType() {
         final Object expected = new Object();
-        final ServiceFactory factory = new StubServiceFactory(expected, "service", Object.class);
+        final ServiceFactory factory = new StubServiceFactory(expected, SERVICE_NAME, Object.class);
         final GenerationContext context = new GenerationContext(Stream.of(factory));
-        final ServiceDescriptor service = new ServiceDescriptor("service", Collections.emptyMap(), Collections.emptyMap());
-        final Object generatedService = context.generate(Object.class, service);
+        final Object generatedService = context.generate(Object.class, SERVICE);
         Assert.assertEquals(expected, generatedService);
     }
 
